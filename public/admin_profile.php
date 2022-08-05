a1edd0c0e395e5e1cc1d39e50b6e0cb913eda59c
Shohruhxon0641 <mey1li2bo0yev3@gmail.com>
2022-08-05 14:43:51 +0500

    create admin profile

---
+++ public/admin_profile.php
@@ -0,0 +1,30 @@
+<?
+include "../layouts/navbar.php";
+?>
+<div class="container-fluid">   
+    <!-- Basic Card Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Basic Card Example</h6>
+            <div class="align-right">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+        The styling for this basic card example is created by using default Bootstrap utility classes. By using utility classes, the style of the card component can be easily modified with no need for any custom CSS!
+        </div>
+    </div>
+</div>
+
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+
+<?
+include "../layouts/header.php";
+include "../layouts/javascrypt.php";
+
+?>
